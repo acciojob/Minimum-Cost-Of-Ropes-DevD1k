37efff3fbfb10ff512c1ad635c6084a8c39456d5
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-04-23 20:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
 function calculateMinCost() {
   //your code here
-   // first of all get elements
- let input=document.getElementById("rope-lengths");
- let inputArr=input.value.split(",");
-
- // Convert string into number
- for(let i=0; i<inputArr.length; i++)
- {
-  inputArr[i]=Number(inputArr[i]);
- }
-
- // Sort array in ascending order
- inputArr.sort(function(a, b) {
-  return a-b;
- })
-
- let totalCost=0;
- // Run loop till array length become 2
- while(inputArr.length>1)
- {
-  let rope1=inputArr[0];
-  let rope2=inputArr[1];
-  let cost=rope1+rope2;
-  totalCost+=cost;
-  
-  // delete first two elements
-  inputArr.splice(0, 2);
-
-  // add cost to array
-  inputArr.push(cost);
-
-  inputArr.sort(function(a, b) {
-    return a-b;
-   })
- }
-
- // Add answer to #result div element
- document.getElementById("result").textContent=totalCost;
-}
+	let form = document.getElementById("form");
+	let ropes = document.getElementById("rope-lengths").value;
+	let result = document.getElementById("result");
+	let ropeLen = ropes.split(',');
+	ropeLen.sort((a,b)=>{return a-b});
+	// console.log(ropes, ropeLen);
+	let all = [];
+	ropeLen.forEach((e)=>{
+		let i = parseInt(e);
+		all.push(i);
+	});
+	let res = 0;
+	while(all.length>1){
+		let a = all.shift();
+		let b = all.shift();
+		res += a+b;
+		all.push(a+b);
+		all.sort((a,b)=>{return a-b});
+	}
+	// console.log(res);
+	result.innerText = res;	
+}  
